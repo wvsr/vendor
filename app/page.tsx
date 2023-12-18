a1b32c10e37b185778cb33b305491832de84cdde
wvsr <email.wasimul@gmail.com>
2023-12-18 23:06:57 +0600

    make input box responsive

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,10 @@ export default function Home() {
     { name: 'About', link: '' },
     { name: 'Store', link: '' },
   ]
+  const mobileHeaderLinks = [
+    { name: 'LLM', link: '' },
+    { name: 'Images', link: '' },
+  ]
   const suggestions = [
     'Images',
     'Videos',
@@ -27,6 +31,7 @@ export default function Home() {
     'Flights',
     'Finance',
   ]
+
   const [searchText, setSearchText] = useState('')
   function handleSearch(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -35,33 +40,60 @@ export default function Home() {
     <main className='search_page h-[92vh] xl:h-screen w-full search_background flex flex-col px-2'>
       <header className='flex justify-between items-center py-4 px-5'>
         <div className='flex gap-3'>
-          <ul className='flex gap-3 justify-center text-center flex-wrap'>
-            {headerLinks.map((e, i) => (
-              <Link
-                href={e.link}
-                key={i}
-                className='text-sm md:text-base text-gray-300'
-              >
-                {e.name}
-              </Link>
-            ))}
-          </ul>
+          <div className='hidden sm:block'>
+            <ul className='flex gap-3 justify-center text-center flex-wrap'>
+              {headerLinks.map((e, i) => (
+                <Link
+                  href={e.link}
+                  key={i}
+                  className='text-sm md:text-base text-gray-300'
+                >
+                  {e.name}
+                </Link>
+              ))}
+            </ul>
+          </div>
+          <div className='block sm:hidden'>
+            <ul className='flex gap-3 justify-center text-center flex-wrap'>
+              {mobileHeaderLinks.map((e, i) => (
+                <Link
+                  href={e.link}
+                  key={i}
+                  className='text-sm md:text-base text-gray-300'
+                >
+                  {e.name}
+                </Link>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className='flex gap-3'>
-          <button className='btn text-sm'>Launch</button>
+          <button className='btn text-sm'>
+            <span className='block lg:hidden'>Sing up</span>
+            <span className='hidden lg:block'>Launch</span>
+          </button>
         </div>
       </header>
       <div className='flex flex-col max-w-5xl mx-auto justify-center items-center flex-1 w-full'>
         <Image
           src={Logo}
           alt='logo'
-          className='mb-7'
-          width={350 * 1.5}
+          className='mb-7 hidden sm:block'
+          width={350 * 1.3}
           height={54}
         />
+        <div className='w-full flex justify-start sm:hidden'>
+          <Image
+            src={Logo}
+            alt='logo'
+            className='mb-7'
+            width={350 * 0.65}
+            height={54}
+          />
+        </div>
         <form className='w-full' onSubmit={handleSearch}>
           <div className='max-w-2xl mx-auto w-full bg-white/10 p-2 rounded-xl overflow-hidden'>
-            <div className='w-full flex bg-white rounded-xl py-2 px-3 overflow-hidden'>
+            <div className='w-full flex bg-white rounded-xl py-1 sm:py-2 px-3 overflow-hidden'>
               <input
                 type='text'
                 className='w-full flex-1 text-base focus:outline-none'
@@ -74,11 +106,22 @@ export default function Home() {
             </div>
           </div>
         </form>
-        <div className='flex gap-3 flex-wrap justify-center mt-4'>
+        <div className='gap-3 flex-wrap justify-center mt-4 hidden sm:flex'>
           {suggestions.map((e, i) => (
             <div
               key={i}
               className='text-gray-400 px-3 py-1 md:py-2 rounded-lg border border-[#032D59] text-xs sm:text-sm'
+              role='button'
+            >
+              {e}
+            </div>
+          ))}
+        </div>
+        <div className='gap-3 flex-wrap justify-start mt-4 flex sm:hidden w-full px-4'>
+          {suggestions.slice(0, 4).map((e, i) => (
+            <div
+              key={i}
+              className='text-gray-400 px-3 py-1.5 md:py-2 rounded-lg border border-[#032D59] text-xs sm:text-sm'
               role='button'
             >
               {e}
